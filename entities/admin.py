67f665a6e99878feb6a61bd37fa6244439f8e138
Shandy G <shandy.gunawan2109@gmail.com>
2021-05-10 20:41:55 +0700

    Register entities models to admin, integrate entities to igog models, migration

--- entities/admin.py
+++ entities/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Supplier, Buyer
 
-# Register your models here.
+#
+# Supplier
+#
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number', 'email', 'address')
+
+
+#
+# Buyer
+#
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number', 'num_ktp', 'num_npwp')
+
+
+#
+# Register
+#
+admin.site.register(Supplier, SupplierAdmin)
+admin.site.register(Buyer, BuyerAdmin)
