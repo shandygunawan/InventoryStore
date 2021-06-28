8a1a9eed40d3a64c32960b1373029c939f08e60d
Shandy G <shandy.gunawan2109@gmail.com>
2021-06-28 19:32:16 +0700

    Add seeder for incoming and outgoing

--- igog/management/commands/seeder.py
+++ igog/management/commands/seeder.py
@@ -1,4 +1,5 @@
 import random
+import string
 from django.core.management.base import BaseCommand, CommandError
 from random import seed
 from random import randint
@@ -10,12 +11,17 @@ from django_seed import Seed
 
 from products.models import Product
 from entities.models import Buyer, Supplier
-from igog.models import Incoming, IncomingProduct
-from igog.models import Outgoing, OutgoingProduct
+from igog.models import (
+    BaseIgog,
+    Incoming,
+    IncomingProduct,
+    Outgoing, OutgoingProduct
+)
 
 class Command(BaseCommand):
     help = "Seed Database"
     AMOUNT = 100
+
 
     def handle(self, *args, **options):
         print('Setting up seed and faker...')
@@ -56,5 +62,63 @@ class Command(BaseCommand):
             )
             p.save()
 
-        # print("Seeding Incoming...")
-        # print("Seeding Outgoing...")
+        len_payment_status = len(BaseIgog.payment_status_choices)
+        len_payment_method = len(BaseIgog.payment_method_choices)
+        len_retrieval_type = len(BaseIgog.retrieval_choices)
+        len_products = Product.objects.all().count()
+        len_suppliers = Supplier.objects.all().count()
+        len_buyers = Buyer.objects.all().count()
+
+        print("Seeding Incoming...")
+        for i in range(self.AMOUNT):
+
+            incoming = Incoming(
+                invoice=fake.swift(),
+                delivery_note=fake.swift(),
+                datetime=fake.date_between(start_date='-30d', end_date='today'),
+                payment_method=BaseIgog.payment_method_choices[randint(0, len_payment_method-1)][0],
+                payment_status=BaseIgog.payment_status_choices[randint(0, len_payment_status-1)][0],
+                # installment_duedate use default (null)
+                installment_fee=randint(1000000, 10000000),
+                note=fake.paragraph(nb_sentences=5),
+                retrieval_type=BaseIgog.retrieval_choices[randint(0, len_retrieval_type-1)][0],
+                # retrieval_date uses default (now)
+                supplier=Supplier.objects.all()[randint(0, len_suppliers-1)]
+            )
+            incoming.save()
+
+            for i in range(5):
+                incoming_product = IncomingProduct(
+                    incoming=incoming,
+                    product=Product.objects.all()[randint(1, len_products-1)],
+                    count=randint(1, 100),
+                    price_per_count=randint(100000, 10000000)
+                )
+                incoming_product.save()
+
+        print("Seeding Outgoing...")
+        for i in range(self.AMOUNT):
+
+            outgoing = Outgoing(
+                invoice=fake.swift(),
+                delivery_note=fake.swift(),
+                datetime=fake.date_between(start_date='-30d', end_date='today'),
+                payment_method=BaseIgog.payment_method_choices[randint(0, len_payment_method-1)][0],
+                payment_status=BaseIgog.payment_status_choices[randint(0, len_payment_status-1)][0],
+                # installment_duedate use default (null)
+                installment_fee=randint(1000000, 10000000),
+                note=fake.paragraph(nb_sentences=5),
+                retrieval_type=BaseIgog.retrieval_choices[randint(0, len_retrieval_type-1)][0],
+                # retrieval_date uses default (now)
+                buyer=Buyer.objects.all()[randint(0, len_buyers-1)]
+            )
+            outgoing.save()
+
+            for i in range(5):
+                outgoing_product = OutgoingProduct(
+                    outgoing=outgoing,
+                    product=Product.objects.all()[randint(1, len_products-1)],
+                    count=randint(1, 100),
+                    price_per_count=randint(100000, 10000000)
+                )
+                outgoing_product.save()
